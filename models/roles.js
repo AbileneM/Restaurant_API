91c0da1b15d4f4e1d0352f0c1857266e8ef8f6df
yanis633 <206932373+yanis5698@users.noreply.github.com>
2026-03-24 02:10:48 -0400

    ajout de commentaires

--- models/roles.js
+++ models/roles.js
@@ -1,17 +1,23 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 
+// Définition du modèle roles
+// Cette table sert à stocker les rôles des utilisateurs
 const Role = sequelize.define("roles", {
+  // Clé primaire de la table roles
   id_role: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true
   },
+
+  // Nom du rôle : admin ou employe
   name: {
     type: DataTypes.STRING,
     allowNull: false
   }
 }, {
+  // Désactive les colonnes createdAt et updatedAt
   timestamps: false
 });
 
