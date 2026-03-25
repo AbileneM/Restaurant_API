2f8a193c8a1b16dccb10e8706bd833a352b6b073
yanis633 <206932373+yanis5698@users.noreply.github.com>
2026-03-24 20:45:19 -0400

    -

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import compression from 'compression';
-import dotenv from 'dotenv'
-dotenv.config()
-import database from './config/connection.js'
-import { getAllUsers } from './controllers/UserController.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import database from './config/db.js';
 import rolesRoutes from './routes/rolesRoutes.js';
 
+
 //Creation de l'application express
-const app = express()
+const app = express();
 
 //Utilisation des middlewares
 app.use(helmet());
 app.use(cors());
 app.use(compression());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,15 +24,14 @@ app.use(express.urlencoded({ extended: true }));
 database.sync({ alter: true })
     .then(() => console.log('La base de données a été synchronisée avec succès.'))
     .catch((error) => console.error('Erreur lors de la synchronisation de la base de données :', error));
+
 //Creation de notre premiere route
+app.get('/premiere-route', (req, res) => res.send('Ceci est ma premiere route avec Express'));
 
-app.get('/premiere-route',(req, res)=>res.send('Ceci est ma premiere route avec Express'));
-app.get('/api/users',getAllUsers)
-
-
-//Route pour les etudiants
+//Routes
 app.use('/api/roles', rolesRoutes);
+
 
 //Demarrage du serveur
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, ()=>console.log(`Le serveur a demarre sur le port ${PORT}`));
+app.listen(PORT, () => console.log(`Le serveur a demarre sur le port ${PORT}`));
