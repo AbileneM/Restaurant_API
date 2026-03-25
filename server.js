f1778e0ce76302053382b5aa0aef0c7e59c28be3
AbileneM <abileneahue@gmail.com>
2026-03-25 05:19:38 -0400

    Correction import db.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import cors from 'cors';
 import compression from 'compression';
 import dotenv from 'dotenv'
 dotenv.config()
-import database from './config/connection.js'
+import connection from './config/db.js';
+import { getAllCategories, getCategorieById, updateCategorie, deleteCategorie, createCategorie } from './controllers/categoriesController.js';
 
 //Creation de l'application express
 const app = express()
@@ -18,11 +19,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Creation des tables dans la base de données
-database.sync({ alter: true })
+connection.sync({ alter: true })
     .then(() => console.log('La base de données a été synchronisée avec succès.'))
     .catch((error) => console.error('Erreur lors de la synchronisation de la base de données :', error));
 
 //Route de test
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'API du restaurant !');
+});
+
+//Routes pour les catégories
+app.get('/api/categories', getAllCategories);
+app.get('/api/categories/:id', getCategorieById);
+app.post('/api/categorie', createCategorie);
+app.put('/api/categories/:id', updateCategorie);
+app.delete('/api/categories/:id', deleteCategorie);
 
 //Demarrage du serveur
 const PORT = process.env.PORT || 4000;
